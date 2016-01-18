630b3ed654d05507a7ddaeec6af040f2b1f0dbff
jamierose <jrose111284@gmail.com>
2016-01-18 11:43:17 +0000

    add balance

---
+++ Gemfile
@@ -0,0 +1,5 @@
+# A sample Gemfile
+source "https://rubygems.org"
+group :development, :test do
+  gem 'rspec'
+end
---
+++ spec/oystercar_spec.rb
@@ -0,0 +1,20 @@
+require 'oystercard'
+describe Oystercard do
+  it 'has a balance of zero' do
+    expect(subject.balance).to eq(0)
+  end
+
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'can top up the balance' do
+      expect{ subject.top_up 1 }.to change { subject.balance }.by (1)
+    end
+
+    it 'raises an error if the maximum balance is exceeded' do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      subject.top_up maximum_balance
+      expect{ subject.top_up 1 }.to raise_error "Maximum balance exceeded"
+    end
+  end
+end
